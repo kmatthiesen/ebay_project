2729220168db6670d544bd302bbdc505321572b9
Kevin Matthiesen <kmatthiesen@catalystitservices.com>
2015-12-18 15:15:58 -0800

    changes

--- Ebay-Project/src/main/resources/public/descrip.js
+++ Ebay-Project/src/main/resources/public/descrip.js
@@ -1,28 +1,28 @@
 var app = angular.module('store-directives', []);
 
-app.directive("descriptions", function() {
-  return {
-    restrict: "E",
-    templateUrl: "descriptions.html"
-  };
-});
-
-// app.directive("productTabs", function() {
+// app.directive("descriptions", function() {
 //   return {
 //     restrict: "E",
-//
-//     templateUrl: "product-tabs.html",
-//     controller: function() {
-//       this.tab = 1;
-//
-//       this.isSet = function(checkTab) {
-//         return this.tab === checkTab;
-//       };
-//
-//       this.setTab = function(activeTab) {
-//         this.tab = activeTab;
-//       };
-//     },
-//     controllerAs: "tab"
+//     templateUrl: "descriptions.html"
 //   };
 // });
+
+app.directive("productTabs", function() {
+  return {
+    restrict: "E",
+
+    templateUrl: "descriptions.html",
+    controller: function() {
+      this.tab = 1;
+
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
+
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: "tab"
+  };
+});
